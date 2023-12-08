a9cdd85c807535abe06dffdd8498a5f352869fed
abdur <abdurleo91@gmail.com>
2023-12-08 23:11:22 +0500

    adding changes

--- image_api/models.py
+++ image_api/models.py
@@ -4,6 +4,7 @@ class Image(models.Model):
     url = models.URLField(null=True, blank=True)
     category = models.CharField(max_length=255)
     name = models.CharField(max_length=255)
+    created_at = models.DateField(auto_created=True)
 
     def __str__(self):
         return self.url
